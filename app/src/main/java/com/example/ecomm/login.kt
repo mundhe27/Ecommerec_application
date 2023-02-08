b2130dd7de576bd181f107e80d9077df742ff3e3
mundhe27 <ashish02.mundhe@gmail.com>
2023-02-08 15:42:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ecomm/login.kt
@@ -0,0 +1,62 @@
+package com.example.ecomm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.FirebaseAuthKtxRegistrar
+import com.google.firebase.ktx.Firebase
+
+class login : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnlogin: Button
+    private lateinit var btnsignup: Button
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnlogin = findViewById(R.id.btn_login)
+        btnsignup = findViewById(R.id.btn_signup)
+
+        btnsignup.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        btnlogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email,password);
+
+        }
+    }
+
+    private fun login(email: String,password: String){
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+                   Toast.makeText(this,"User does not exist",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+}
